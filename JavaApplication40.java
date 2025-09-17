c70220da4c6e83732f7f16319f723b7bfa61c687
dhanapedrosa <studihsan@gmail.com>
2025-09-17 20:09:29 +0700

    first commit

---
+++ JavaApplication40.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication40;
+
+/**
+ *
+ * @author Provicom
+ */
+public class JavaApplication40 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Belajar algoritma Pemrograman");
+        System.out.println("");
+        System.out.println("rrrrrrr");
+        System.out.println("hgjgj");
+        String nama ="dhana";
+        System.out.println(nama);
+    }
+    
+}
